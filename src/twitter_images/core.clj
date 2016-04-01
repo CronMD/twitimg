51aaeb05521a6b6301170ec45357da66eab68646
CronMD <mightycron@gmial.com>
2016-04-01 16:33:38 +0500

    Refactored

--- src/twitter_images/core.clj
+++ src/twitter_images/core.clj
@@ -3,15 +3,8 @@
             [clojure.data.json :as json])
 )
 
-;(re-find #"src=\"https:\/\/pbs\.twimg\.com\/media\/(.*)\"\s" s)
-;(re-seq #"src=\"https:\/\/pbs\.twimg\.com\/media\/(.*)\"\s alt" (get-media "openitpro"))
-
 (defn get-images [html]
 	(map #(str "https://pbs.twimg.com/media/" (% 1)) (re-seq #"src=\"https:\/\/pbs\.twimg\.com\/media\/(.*)\"\salt" html)) ;"
-	; (if (nil? html)
-	; 	'()
-	; 	(map #(str "https://pbs.twimg.com/media/" (% 1)) (re-seq #"src=\"https:\/\/pbs\.twimg\.com\/media\/(.*)\"\salt" html)) ;"	
-	; )
 )
 
 (defn get-position [html]
@@ -26,15 +19,6 @@
 			images (into [] images-list)
 			max-position (get-position html)
 		 ]
-
-
-		 ; (let [
-		 ; 			url (str "https://twitter.com/i/profiles/show/openitpro/media_timeline?include_available_features=1&include_entities=1&max_position=" 538643097417416704 "&reset_error_state=false")
-		 ; 		 	data (json/read-str ((client/get url) :body))
-		 ; 	  ]
-		 ; 	  [(data "items_html") (data "min_position") (data "has_more_items")]
-		 ; )
-
 
 		 (loop [pos max-position images images]
 		 	(let [
@@ -51,8 +35,6 @@
 		 )
 	)
 )
-
-
 
 (defn -main[] 
 	(println "Hello")
